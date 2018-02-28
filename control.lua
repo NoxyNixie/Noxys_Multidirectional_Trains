17ed0072e5c11f0ad83025bd48ac05aa87ad1902
Mark Sanders <mark.something@gmail.com>
2018-02-28 04:30:29 +0100

    Made it all work with the new .speed attribute for rolling stock.

--- control.lua
+++ control.lua
@@ -1,31 +1,15 @@
--- If only you could rotate locomotives that where connected this mod would be so much easier.
-
-local movingstate = {
-	[defines.train_state.on_the_path] = true,
-	[defines.train_state.path_lost] = false,
-	[defines.train_state.no_schedule] = false,
-	[defines.train_state.no_path] = false,
-	[defines.train_state.arrive_signal] = true,
-	[defines.train_state.wait_signal] = true,
-	[defines.train_state.arrive_station] = true,
-	[defines.train_state.wait_station] = false,
-	[defines.train_state.manual_control_stop] = false,
-	[defines.train_state.manual_control] = false,
-}
-
 local function rotate(loco)
+	-- @todo: This is a hack since you can't rotate stock when it is connected.
 	local back = loco.disconnect_rolling_stock(defines.rail_direction.back)
 	local front = loco.disconnect_rolling_stock(defines.rail_direction.front)
-	local ret = loco.rotate()
-	-- @todo: Check if this needs to be inversed due to rotate or not?
-	if back then loco.connect_rolling_stock(defines.rail_direction.back) end
-	if front then loco.connect_rolling_stock(defines.rail_direction.front) end
-	return ret
+	loco.rotate()
+	if front then loco.connect_rolling_stock(defines.rail_direction.back) end
+	if back then loco.connect_rolling_stock(defines.rail_direction.front) end
 end
 
 script.on_init(function()
-	global.movingstate = global.movingstate or {}
-	global.lastmode = global.lastmode or {}
+	global.movingstate = {}
+	global.lastmode = {}
 end)
 
 script.on_event(defines.events.on_train_created, function (event)
@@ -35,7 +19,6 @@ script.on_event(defines.events.on_train_created, function (event)
 		global.movingstate[event.old_train_id_1] = nil
 		global.lastmode[event.train.id] = global.lastmode[event.old_train_id_1]
 		global.lastmode[event.old_train_id_1] = nil
-		--@todo: current schedule id too?
 	else
 		-- Train created
 		global.movingstate[event.train.id] = false
@@ -43,87 +26,39 @@ script.on_event(defines.events.on_train_created, function (event)
 	end
 end)
 
-script.on_nth_tick(10, function()
+script.on_nth_tick(15, function()
 	local trains = game.surfaces[1].get_trains()
 	for _,train in pairs(trains) do
-		local moving = train.speed ~= 0 --@todo: Maybe see slow moving as stopped as well?
-		if moving ~= global.movingstate[train.id] then
-			global.movingstate[train.id] = moving
-			global.lastmode[train.id] = train.manual_mode
-			if moving then
-				-- figure out which locos are facing the wrong way
-				-- I currently know of no way to reliably tell which locomotives are moving forward and visa versa.
-				game.print("f: " .. train.front_stock.unit_number .. " b: " .. train.back_stock.unit_number)
-				for _,w in pairs(train.locomotives) do
-					for k,v in pairs(w) do
-						game.print(k .. ":" .. v.unit_number .. ": " .. v.orientation)
+		if not train.manual_mode then
+			local id = train.id
+			local moving = train.speed ~= 0
+			if moving ~= global.movingstate[id] then
+				local global = global
+				global.movingstate[id] = moving
+				global.lastmode[id] = train.manual_mode
+				if moving then -- Started moving: figure out which locos are facing the wrong way.
+					for _,w in pairs(train.locomotives) do
+						for k,loco in pairs(w) do
+							if loco.speed < 0 then
+								global[loco.unit_number] = true
+								rotate(loco)
+								train = loco.train
+							end
+						end
+					end
+				else -- No longer moving. Revert the train to its neutral state.
+					for _, locos in pairs(train.locomotives) do
+						for _, loco in pairs(locos) do
+							if global[loco.unit_number] then
+								rotate(loco)
+								global[loco.unit_number] = nil
+								train = loco.train
+							end
+						end
 					end
 				end
-				for _,v in pairs(train.carriages) do
-					if v.type == "locomotive" then
-						game.print(v.unit_number .. ": " .. v.orientation)
-					end
-				end
-			else
-				for _, loco in pairs(train.locomotives.back_movers) do
-					if global[loco.unit_number] then
-						rotate(loco)
-						global[loco.unit_number] = nil
-						train = loco.train
-					end
-				end
-				for _, loco in pairs(train.locomotives.front_movers) do
-					if global[loco.unit_number] then
-						rotate(loco)
-						global[loco.unit_number] = nil
-						train = loco.train
-					end
-				end
-				train.manual_mode = global.lastmode[train.id]
+				train.manual_mode = global.lastmode[id]
 			end
 		end
 	end
 end)
-
--- @todo: I may have to do this stuff in on-tick and just check train speed/direction then
---[[
-script.on_event(defines.events.on_train_changed_state, function(event)
-	local train = event.train
-	if train.state == defines.train_state.manual_control or train.state == defines.train_state.manual_control_stop then
-		return
-	end
-	if event.old_state == defines.train_state.manual_control or event.old_state == defines.train_state.manual_control_stop then
-		return
-	end
-	game.print("f:" .. train.front_stock.unit_number)
-	game.print("b:" .. train.back_stock.unit_number)
-	if movingstate[train.state] ~= global.movingstate[train.id] then
-		global.movingstate[train.id] = movingstate[train.state]
-		global.lastmode[train.id] = train.manual_mode
-		if movingstate[train.state] then
-			for _, loco in pairs(train.locomotives.back_movers) do
-				game.print("l" .. loco.unit_number .. ":" .. serpent.line(loco.train.riding_state))
-				global[loco.unit_number] = rotate(loco)
-				train = loco.train
-			end
-			train.manual_mode = global.lastmode[train.id]
-		else
-			for _, loco in pairs(train.locomotives.back_movers) do
-				if global[loco.unit_number] then
-					rotate(loco)
-					global[loco.unit_number] = nil
-					train = loco.train
-				end
-			end
-			for _, loco in pairs(train.locomotives.front_movers) do
-				if global[loco.unit_number] then
-					rotate(loco)
-					global[loco.unit_number] = nil
-					train = loco.train
-				end
-			end
-			train.manual_mode = global.lastmode[train.id]
-		end
-	end
-end)
---]]
