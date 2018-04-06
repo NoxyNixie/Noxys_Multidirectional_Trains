73d887e49694179f53404862099b411be9942976
Mark Sanders <mark.something@gmail.com>
2018-04-07 01:46:22 +0200

    Changed the logic used to read and re-apply the correct mode for a train (Fixes: #4). Changed it so after rotation both sides are tried to be reconnected no matter if they weren't connected before (this might fix #3 but this is untested). Reverted the change by ChuckSavage as this was not the desired effect.

--- control.lua
+++ control.lua
@@ -1,15 +1,15 @@
 local function rotate(loco)
 	-- @todo: This is a hack since you can't rotate stock when it is connected.
-	local back = loco.disconnect_rolling_stock(defines.rail_direction.back)
-	local front = loco.disconnect_rolling_stock(defines.rail_direction.front)
+	loco.disconnect_rolling_stock(defines.rail_direction.back)
+	loco.disconnect_rolling_stock(defines.rail_direction.front)
 	loco.rotate()
-	if front then loco.connect_rolling_stock(defines.rail_direction.back) end
-	if back then loco.connect_rolling_stock(defines.rail_direction.front) end
+	-- Try to reconnect
+	loco.connect_rolling_stock(defines.rail_direction.back)
+	loco.connect_rolling_stock(defines.rail_direction.front)
 end
 
 script.on_init(function()
 	global.movingstate = {}
-	global.lastmode = {}
 end)
 
 script.on_event(defines.events.on_train_created, function (event)
@@ -17,27 +17,25 @@ script.on_event(defines.events.on_train_created, function (event)
 		-- Train changed
 		global.movingstate[event.train.id] = global.movingstate[event.old_train_id_1]
 		global.movingstate[event.old_train_id_1] = nil
-		global.lastmode[event.train.id] = global.lastmode[event.old_train_id_1]
-		global.lastmode[event.old_train_id_1] = nil
 	else
 		-- Train created
 		global.movingstate[event.train.id] = false
-		global.lastmode[event.train.id] = true
 	end
 end)
 
 script.on_nth_tick(15, function()
 	local trains = game.surfaces[1].get_trains()
 	for _,train in pairs(trains) do
-		if not train.manual_mode then
-			local id = train.id
-			local rotated = false
-			local moving = train.speed ~= 0
-			if moving ~= global.movingstate[id] then
-				local global = global
-				global.movingstate[id] = moving
-				global.lastmode[id] = train.manual_mode
-				if moving then -- Started moving: figure out which locos are facing the wrong way.
+		local id = train.id
+		local rotated = false
+		local moving = train.speed ~= 0
+		if moving ~= global.movingstate[id] then
+			local global = global
+			global.movingstate[id] = moving
+			local manual_mode = train.manual_mode
+			game.print("Mode Pre : " .. tostring(manual_mode))
+			if moving then -- Started moving: figure out which locos are facing the wrong way.
+				if not manual_mode then
 					for _,w in pairs(train.locomotives) do
 						for _,loco in pairs(w) do
 							if loco.speed < 0 then
@@ -48,21 +46,22 @@ script.on_nth_tick(15, function()
 							end
 						end
 					end
-				else -- No longer moving. Revert the train to its neutral state.			
-					for _, locos in pairs(train.locomotives) do
-						for _, loco in pairs(locos) do
-							if global[loco.unit_number] then
-								rotate(loco)
-								rotated = true
-								global[loco.unit_number] = nil
-								train = loco.train
-							end
+				end
+			else -- No longer moving. Revert the train to its neutral state.
+				for _, locos in pairs(train.locomotives) do
+					for _, loco in pairs(locos) do
+						if global[loco.unit_number] then
+							rotate(loco)
+							rotated = true
+							global[loco.unit_number] = nil
+							train = loco.train
 						end
 					end
 				end
-				if rotated then
-					train.manual_mode = global.lastmode[id]
-				end
+			end
+			if rotated then
+				game.print("Mode Post: " .. tostring(manual_mode))
+				train.manual_mode = manual_mode
 			end
 		end
 	end
