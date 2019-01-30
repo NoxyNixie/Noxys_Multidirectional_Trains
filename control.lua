d2c1b861fdec0556c53043deb0425c9d69aa2209
Optera <yousei.kyoko+github@gmail.com>
2019-01-30 20:50:07 +0100

    cleaned up debug code

--- control.lua
+++ control.lua
@@ -27,7 +27,7 @@ end)
 ---- ROTATE LOGIC ----
 do
 local function rotate(loco)
-  log("DEBUG: rotating "..tostring(loco.backer_name) )
+  -- log("DEBUG: rotating "..tostring(loco.backer_name) )
   local disconnected_back = loco.disconnect_rolling_stock(defines.rail_direction.back)
   local disconnected_front = loco.disconnect_rolling_stock(defines.rail_direction.front)
   loco.rotate()
@@ -117,34 +117,8 @@ end
 
 ---- TRAIN STATE CHANGED ----
 do
-
-local train_state_definitions = {
-  [0] = "on_the_path", -- Normal state -- following the path.",
-  [1] = "path_lost", -- Had path and lost it -- must stop.",
-  [2] = "no_schedule", -- Doesn't have anywhere to go.",
-  [3] = "no_path", -- Has no path and is stopped.",
-  [4] = "arrive_signal", -- Braking before a rail signal.",
-  [5] = "wait_signal", --	Waiting at a signal.",
-  [6] = "arrive_station", --	Braking before a station.",
-  [7] = "wait_station", --	Waiting at a station.",
-  [8] = "manual_control_stop", --	Switched to manual control and has to stop.",
-  [9] = "manual_control", --	Can move if user explicitly sits in and rides the train.",
-}
-
-function GetMainLocomotive(train)
-  if train.valid and train.locomotives and (#train.locomotives.front_movers > 0 or #train.locomotives.back_movers > 0) then
-    return train.locomotives.front_movers and train.locomotives.front_movers[1] or train.locomotives.back_movers[1]
-  end
-end
-
-function GetTrainName(train)
-  local loco = GetMainLocomotive(train)
-  return loco and loco.backer_name
-end
-
 function OnTrainStateChanged(event)
   local train = event.train
-  -- log("(OnTrainStateChanged) Train name: "..tostring(GetTrainName(train))..", train.id:"..tostring(train.id).." stop: "..tostring(train.station and train.station.backer_name)..", state: "..tostring(train_state_definitions[event.old_state]).." > "..tostring(train_state_definitions[train.state])..", train.manual_mode: "..tostring(train.manual_mode) )
   if train.state == defines.train_state.wait_station or (train.manual_mode and event.old_state <= 8) then -- wait or automatic > manual
     -- players can hop into waiting trains, switch to manual and start driving
     -- without clearing global.trains_to_rotate nth_rick would incorrectly rotate shortly afterwards
@@ -152,7 +126,7 @@ function OnTrainStateChanged(event)
     if not next(global.trains_to_rotate) then
       script.on_nth_tick(nil)
     end
-    
+
     UnrotateTrain(train)
 
   elseif not train.manual_mode and (event.old_state == defines.train_state.wait_station or event.old_state == defines.train_state.manual_control) then -- wait > automatic or manual > automatic
@@ -163,11 +137,6 @@ function OnTrainStateChanged(event)
     end
   end
 end
-
-
--- function OnTrainCreated(event)
-  -- log("(OnTrainCreated) Train name: "..tostring(GetTrainName(event.train))..", train.id:"..tostring(event.train.id)..", .old_train_id_1:"..tostring(event.old_train_id_1)..", .old_train_id_2:"..tostring(event.old_train_id_2)..", state: "..tostring(train_state_definitions[event.train.state]))
--- end
 
 end
 
@@ -182,7 +151,7 @@ function OnNthTick(NthTickEvent)
     else
       -- remove invalid train
       global.trains_to_rotate[trainID] = nil
-      log("DEBUG: removed invalid train "..trainID)
+      -- log("DEBUG: removed invalid train "..trainID)
     end
   end
 
@@ -196,7 +165,6 @@ end
 do
 local function init_events()
   script.on_event(defines.events.on_train_changed_state, OnTrainStateChanged)
-  -- script.on_event(defines.events.on_train_created, OnTrainCreated)
   if next(global.trains_to_rotate) then
     script.on_nth_tick(Nth_tick, OnNthTick)
   end
