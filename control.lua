4681e1734daf540529a987617543ed5c6b59acac
Kira <kirazy@live.com>
2024-11-30 21:49:28 -0800

    feat: add events for rotating/unrotating

--- control.lua
+++ control.lua
@@ -1,8 +1,14 @@
+require("scripts.remote-interface")
+
 local settings_enabled = settings.global["Noxys_Multidirectional_Trains-enabled"].value
 local settings_nth_tick = settings.global["Noxys_Multidirectional_Trains-on_nth_tick"].value
 local settings_station_limits = settings.global["Noxys_Multidirectional_Trains-station_limits"].value
 
+---Rotates the given locomotive.
+---@param loco LuaEntity
 local function rotate(loco)
+  local old_train_id = loco.train and loco.train.id
+
   -- todo: This is a hack since you can't rotate stock when it is connected.
   local disconnected_back = loco.disconnect_rolling_stock(defines.rail_direction.back)
   local disconnected_front = loco.disconnect_rolling_stock(defines.rail_direction.front)
@@ -13,32 +19,49 @@ local function rotate(loco)
   if disconnected_front then
     loco.connect_rolling_stock(defines.rail_direction.back)
   end
-  -- Error handling removed since a meaningfull error message is difficult (or too noisy) to produce.
+
+  raise_train_locomotive_rotated(loco.train, old_train_id)
+  -- Error handling removed since a meaningful error message is difficult (or too noisy) to produce.
 end
 
--- Rotate all locomotives to face driving direction, rotated locomotives are added to storage.rotated_locos.
-local function train_rotate(train)
+---Rotate all locomotives to face driving direction, rotated locomotives are added to `storage.rotated_locos`.
+---@param train LuaTrain
+---@param old_state defines.train_state?
+local function train_rotate(train, old_state)
   local schedule_index = train.schedule.current
   local manual_mode = train.manual_mode
   if manual_mode then return end -- never rotate manual mode trains
+
+  local has_raised_rotate_started_event = false
+  local old_train_id = train.id
+
   for _, locos in pairs(train.locomotives) do
     for _, loco in pairs(locos) do
+      ---@cast loco LuaEntity
       if not storage.rotated_locos[loco.unit_number] and loco.speed < 0 then -- prevent double rotates
+        if not has_raised_rotate_started_event then -- raise event only if the train is going to rotate.
+          raise_train_rotating(train, old_state)
+          has_raised_rotate_started_event = true
+        end
         storage.rotated_locos[loco.unit_number] = true
         rotate(loco)
         train = loco.train -- Ensure that this reference is valid for restoring manual mode.
       end
     end
   end
-  if train.manual_mode ~= manual_mode then
+  if train and train.manual_mode ~= manual_mode then
     train.manual_mode = manual_mode
   end
-  if schedule_index and train.valid then
+  if schedule_index and train and train.valid then
     train.go_to_station(schedule_index)
+  end
+
+  if has_raised_rotate_started_event then -- raise event only if the train was rotated.
+    raise_on_train_rotated(train, old_train_id, old_state)
   end
 end
 
--- Hack to get locomotive orientation through speed some ticks after it started moving.
+---Hack to get locomotive orientation through speed some ticks after it started moving.
 local function on_nth_tick()
   for trainID, train in pairs(storage.trains_to_rotate) do
     if train.valid then
@@ -62,7 +85,8 @@ local function on_nth_tick()
   end
 end
 
--- Revert the rotated locomotives listed in storage.rotated_locos.
+---Revert the rotated locomotives listed in `storage.rotated_locos`.
+---@param train LuaTrain
 local function train_unrotate(train)
   local schedule_index = train.schedule.current
   local manual_mode = train.manual_mode
@@ -72,20 +96,33 @@ local function train_unrotate(train)
     storage.station_limits[station.unit_number] = station
     script.on_nth_tick(settings_nth_tick, on_nth_tick)
   end
+
+  local has_raised_rotate_started_event = false
+  local old_train_id = train.id
+
   for _, locos in pairs(train.locomotives) do
     for _, loco in pairs(locos) do
+      ---@cast loco LuaEntity
       if storage.rotated_locos[loco.unit_number] then
+        if not has_raised_rotate_started_event then -- raise event only if the train is going to rotate.
+          raise_train_rotating(train)
+          has_raised_rotate_started_event = true
+        end
         rotate(loco)
         storage.rotated_locos[loco.unit_number] = nil
         train = loco.train -- Ensure that this reference is valid for restoring manual mode.
       end
     end
   end
-  if train.manual_mode ~= manual_mode then
+  if train and train.manual_mode ~= manual_mode then
     train.manual_mode = manual_mode
   end
-  if schedule_index and train.valid then
+  if schedule_index and train and train.valid then
     train.go_to_station(schedule_index)
+  end
+
+  if has_raised_rotate_started_event then -- raise event only if the train was rotated.
+    raise_on_train_rotated(train, old_train_id)
   end
 end
 
@@ -93,7 +130,6 @@ local function on_train_changed_state(event)
   local train = event.train
   if train.state == defines.train_state.wait_station or
       train.state == defines.train_state.no_path or
-      train.state == defines.train_state.path_lost or
       (train.manual_mode and event.old_state ~= defines.train_state.manual_control_stop and
         event.old_state ~= defines.train_state.manual_control)
   then
@@ -101,6 +137,9 @@ local function on_train_changed_state(event)
     if not next(storage.trains_to_rotate) and not next(storage.station_limits) then
       script.on_nth_tick(nil)
     end
+    --Raise prior to unrotating to ensure train state at time of unrotation is available to subscribers.
+    raise_train_unrotating(train, event.old_state)
+
     train_unrotate(train)
   elseif not train.manual_mode and
       (event.old_state == defines.train_state.wait_station or
